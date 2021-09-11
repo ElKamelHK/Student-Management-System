1617f841d5b857a192fe6b7fd4e4abeeb1823fd9
ElKamelHK <hamdiek58@gmail.com>
2021-09-11 20:28:56 +0100

    create message update

--- src/main/java/com/studentManagementSystem/controller/StudentController.java
+++ src/main/java/com/studentManagementSystem/controller/StudentController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.studentManagementSystem.model.Student;
 import com.studentManagementSystem.service.StudentService;
@@ -42,41 +43,47 @@ public class StudentController {
 	// Création d'un nouveau etudiant
 	@GetMapping("/new")
 	public String createStudentForm(Model model) {
-		// create un etudiant 
-		Student student =new Student();
+		// create un etudiant
+		Student student = new Student();
 		model.addAttribute("student", student);
 		return "create_student";
 
 	}
-	
+
 	@PostMapping("/save")
-	public String saveStudent(@ModelAttribute("student") Student student)
-	{
+	public String saveStudent(@ModelAttribute("student") Student student) {
 		this.studentService.saveStudent(student);
 		return "redirect:/api/student/all";
 	}
-	//create update student view
+
+	// create update student view
 	@GetMapping("/update/{id}")
-	public String updateStudentForm(Model model,@PathVariable("id") Long id)
-	{
-		Optional<Student> opStudent=this.studentService.optionalStudentById(id);
-		if(!opStudent.isPresent())
+	public String updateStudentForm(Model model, @PathVariable("id") Long id) {
+		Optional<Student> opStudent = this.studentService.optionalStudentById(id);
+		if (!opStudent.isPresent())
 			return "404";
-		else
-		{
+		else {
 			model.addAttribute("student", opStudent.get());
 			return "update_student";
 		}
-		
+
 	}
-	 
+
 	@PostMapping("/update")
-	public String updateStudent(@ModelAttribute("student") Student student )
-	{
-		System.out.println(student);
-		this.studentService.updateStudent(student);
-		 
-		return "redirect:/api/student/all"; 
+	public String updateStudent(@ModelAttribute("student") Student student, RedirectAttributes redirectAttributes) {
+
+		Student st = this.studentService.updateStudent(student);
+		
+		if (st == null) {
+			redirectAttributes.addFlashAttribute("message", "THIS STUDENT NOT FOUND BLEASE VERIFY YOUR REQUEST !!");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			return "redirect:/api/student/update/" + student.getId();
+		}
+
+		redirectAttributes.addFlashAttribute("message", "Success");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		return "redirect:/api/student/update/" + student.getId();
+
 	}
 
 }
